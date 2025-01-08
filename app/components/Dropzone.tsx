2a71e2781a2785104004608cc2b2a01d3d2fff5d
priyanshilohani <111214902+priyanshilohani@users.noreply.github.com>
2025-01-08 22:57:09 +0530

    Add files via upload

---
+++ app/components/Dropzone.tsx
@@ -0,0 +1,72 @@
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+export default function Dropzone({
+  setPdfFile,
+  setFile,
+}: {
+  setPdfFile: (file: string | null) => void;
+  setFile: (file: File | null) => void;
+}) {
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setErrorMessage("");
+
+    if (acceptedFiles.length != 1) {
+      setErrorMessage("Please upload only one file");
+      return;
+    }
+
+    const file = acceptedFiles[0];
+    if (file.type !== "application/pdf") {
+      setErrorMessage("Please upload a PDF");
+      return;
+    }
+
+    setLoading(true);
+    const formData = new FormData();
+    formData.set("file", file);
+    const response = await fetch("/api/addFile", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      setErrorMessage("Error during uploading file");
+      setLoading(false);
+      return;
+    }
+    await response.json();
+    setLoading(false);
+    setPdfFile(file.name);
+    setFile(file);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div>
+      {loading ? (
+        <div className="border-dashed border-2 border-gray-300 p-4 mb-4 text-center">
+          Loading...
+        </div>
+      ) : (
+        <div
+          {...getRootProps()}
+          className="border-dashed border-2 border-gray-300 p-4 mb-4 text-center"
+        >
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the file here ...</p>
+          ) : (
+            <p>Drag and drop the file here, or click to select the file</p>
+          )}
+        </div>
+      )}
+      {errorMessage && (
+        <p className="text-red-600 text-center">{errorMessage}</p>
+      )}
+    </div>
+  );
+}
